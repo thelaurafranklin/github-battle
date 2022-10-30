ed0e1a62c758c56030be918587de5f2529923ab7
thelaurafranklin <thelaurafranklin@gmail.com>
2022-10-29 17:33:57 -0700

    add popular repo table

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import PropTypes from 'prop-types';
 import { fetchPopularRepos } from '../utils/api';
+import Table from './Table';
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -19,7 +20,7 @@ function LanguagesNav({ selected, onUpdateLanguage }) {
   );
 }
 
-LanguagesNav.PropTypes = {
+LanguagesNav.propTypes = {
   selected: PropTypes.string.isRequired,
   onUpdateLanguage: PropTypes.func.isRequired,
 };
@@ -75,7 +76,7 @@ export default class Popular extends React.Component {
         </div>
 
         {error && <p className="text-center error">{error}</p>}
-        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+        {repos && <Table repos={repos} />}
       </main>
     );
   }
